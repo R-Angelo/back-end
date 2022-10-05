791fd09512daa6b46bd269d50473a3eda5147f5a
rayveerrr <95725971+rayveerrr@users.noreply.github.com>
2022-10-05 16:57:43 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,27 @@
+
+loginFN = () => {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    const arr = 
+    [
+        {
+            id: "1",
+            email: "admin",
+            password: "1234"
+        },
+        {
+            id: "2",
+            email: "user",
+            password: "user"
+        },
+        {
+            id: "3",
+            email: "1234",
+            password: "1234"
+        }
+    
+    ]
+            
+    
+}
