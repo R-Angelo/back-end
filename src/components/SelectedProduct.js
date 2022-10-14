b4e32919940063837fbee60f3089d246b1a8a09d
rayveerrr <95725971+rayveerrr@users.noreply.github.com>
2022-10-14 09:41:13 +0800

    Update SelectedProduct.js

--- src/components/SelectedProduct.js
+++ src/components/SelectedProduct.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 //components
@@ -13,7 +14,27 @@ import beefpro from '../image/beefpro.jpg'
 // styles
 import '../styles/selectedproduct.css'
 
+// icons
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button'
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import Stack from '@mui/material/Stack';    
+import { Input, styled } from '@mui/material'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { withTheme } from 'styled-components'
+
+
 const SelectedProduct = () => {
+    const [quantityCount, setQuantityCount] = useState(0);
+
+    const increment = () =>{
+        setQuantityCount(quantityCount + 1);
+    }
+    const decrement = () =>{
+        setQuantityCount(quantityCount - 1);
+    }
+    
     return (
         <div className='main'>
         <Header />
@@ -30,10 +51,26 @@ const SelectedProduct = () => {
                 <div className="availability-container">
                     <h1>Beef pro adult</h1>
                     <p>Price: 3,000</p>
-                    <p>Quantity: <input type="number" name="" id="" /> </p>
+                    <p> <Stack direction="row" alignItems='center'> Quantity:
+                    <IconButton aria-label="minus" onClick={decrement} disabled={quantityCount == 0} >
+                        <RemoveIcon fontSize='small' sx={{ backgroundColor: 'white', color: 'black'}} />
+                    </IconButton>
+                        <Input sx={{ backgroundColor: 'white'}} placeholder='0' value={quantityCount}></Input>
+                    <IconButton aria-label="add" onClick={increment} >
+                        <AddIcon fontSize='small' sx={{ backgroundColor: 'white', color: 'black'}}/>
+                    </IconButton>
+                        <IconButton variant="contained" endIcon={<AddIcon />} />
+                    </Stack> 
+                    </p>
                     <div className="button">
-                        <button>Add to cart</button>
-                        <button>Buy now</button>
+                        <Button 
+                            variant='contained' 
+                            type='submit' 
+                            endIcon={<ShoppingCartIcon />}
+                            sx={{backgroundColor: 'white', color: 'black'}}
+                        >
+                            Add to cart
+                        </Button>
                     </div>
                     <div className="payment-method">
                         <h3>Payment method</h3>
