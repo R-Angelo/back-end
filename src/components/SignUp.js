6ca81ab40505d170b06966c27788cab6aa7d14c4
rayveerrr <95725971+rayveerrr@users.noreply.github.com>
2022-10-20 10:37:31 +0800

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Tools
 import { Avatar, FormControlLabel, Grid, Paper, TextField, Checkbox, Button, Typography, Link } from '@mui/material';
@@ -9,14 +9,59 @@ import FormLabel from '@mui/material/FormLabel';
 
 //Components
 import LoginHeader from './LoginHeader'
-import Footer from "./Footer";
 
 //icons
 import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 
 const SignUp = () =>{
+
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [phoneNum, setPhoneNum] = useState();
+    const [password, setPassword] = useState();
+    const [cpass, setCPass] = useState();
+
+    // Error
+    const [nameError, setNameError] = useState();
+    const [emailError, setEmailError] = useState();
+    const [phoneNumError, setPhoneNumError] = useState();
+    const [passwordError, setPasswordError] = useState();
+    const [cpassError, setCPassError] = useState();
+
+    const [gender, setGender] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setName(false)
+        setEmail(false)
+        setPhoneNum(false)
+        setPassword(false)
+        setCPass(false)
+
+        if( name == ''){
+            setNameError(true)
+        }
+        if( email == ''){
+            setEmailError(true)
+        }
+        if( phoneNum == ''){
+            setPhoneNumError(true)
+        }
+        if( password == ''){
+            setPasswordError(true)
+        }
+        if( cpass == ''){
+            setCPassError(true)
+        }
+
+
+        if (name && email && phoneNum && password && cpass){
+            console.log(name, email, gender, phoneNum, password, cpass)
+        }
+    }
+
     const paperStyled = {
-        padding: 20, height: '70vh', width: 500, margin: '100px auto'
+        padding: 20, height: '70vh', width: 500, margin: '100px auto', color: "black" 
     }
     const btnStyle={
         marginBottom: 10
@@ -44,15 +89,33 @@ const SignUp = () =>{
                 </Typography>
             </Grid>
             <Grid>
-                <form>
-                    <TextField variant="outlined" id="name" placeholder="Name" fullWidth required style={txtFieldStyle}/>
-                    <TextField variant="outlined" id="email" placeholder="Email" fullWidth required style={txtFieldStyle}/>
+                <form onSubmit={handleSubmit}>
+                    <TextField 
+                        variant="outlined" 
+                        label='Name' 
+                        id="name" 
+                        fullWidth
+                        onChange={(e) => setName(e.target.value)}
+                        error={nameError}
+                        required 
+                        style={txtFieldStyle} />
+                    <TextField 
+                        variant="outlined" 
+                        label='Email' 
+                        id="email" 
+                        fullWidth 
+                        onChange={(e) => setEmail(e.target.value)}
+                        error={emailError}
+                        required 
+                        style={txtFieldStyle}/>
                     <FormControl>
                         <FormLabel id="gender">Gender</FormLabel>
                         <RadioGroup
                             row
                             aria-labelledby="gender"
                             name="gender"
+                            value={gender}
+                            onChange={(e) => {setGender(e.target.value)}}
                         >
                             <FormControlLabel value="female" control={<Radio />} label="Female" />
                             <FormControlLabel value="male" control={<Radio />} label="Male" />
@@ -66,11 +129,35 @@ const SignUp = () =>{
                             /> */}
                         </RadioGroup>
                     </FormControl>
-                    <TextField variant="outlined" id="phoneNum" placeholder="Phone number" fullWidth required style={txtFieldStyle}/>
-                    <TextField variant="outlined" id="password" placeholder="Password" fullWidth required style={txtFieldStyle} />
-                    <TextField variant="outlined" id="confirmPassword" placeholder="Confirm Password" fullWidth required style={txtFieldStyle} />
-                    <FormControlLabel control={<Checkbox defaultChecked />} label="I accept the terms and conditions" />
-                    <Button variant='contained' fullWidth style={btnStyle}>Sign Up</Button>
+                    <TextField 
+                        variant="outlined" 
+                        label='Phone number' 
+                        id="phoneNum" 
+                        fullWidth 
+                        onChange={(e) => setPhoneNum(e.target.value)}
+                        error={phoneNumError}
+                        required 
+                        style={txtFieldStyle}/>
+                    <TextField 
+                        variant="outlined" 
+                        label='Password' 
+                        id="password" 
+                        fullWidth 
+                        onChange={(e) => setPassword(e.target.value)}
+                        error={passwordError}
+                        required 
+                        style={txtFieldStyle} />
+                    <TextField 
+                        variant="outlined" 
+                        label='Confirm Password' 
+                        id="confirmPassword" 
+                        fullWidth
+                        onChange={(e) => setCPass(e.target.value)}
+                        error={cpassError} 
+                        required 
+                        style={txtFieldStyle} />
+                    <FormControlLabel control={<Checkbox required/>} label="I accept the terms and conditions"/>
+                    <Button type="submit" variant='contained' fullWidth style={btnStyle}>Sign Up</Button>
                     <hr></hr>
                     <Typography align='center'>
                         Already have an account? <Link href="/login"> Sign in</Link>
@@ -79,7 +166,6 @@ const SignUp = () =>{
             </Grid>
         </Paper>
     </Grid>
-    <Footer />
         </>
     )
 }
