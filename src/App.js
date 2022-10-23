f72ff9be28e9099c2d729c8d945595c72091129f
rayveerrr <95725971+rayveerrr@users.noreply.github.com>
2022-10-24 00:10:22 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import React, {useState} from 'react';
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
-import "./style/index.css";
+import "./styles/index.css";
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'Order ID', width: 500 },
+  { field: 'id', headerName: 'Order ID', width: 80 },
+  { field: 'status', headerName: 'Order Status', width: 130 },
+  { field: 'status', headerName: 'Order Status', width: 130 },
+  { field: 'status', headerName: 'Order Status', width: 130 },
+  { field: 'status', headerName: 'Order Status', width: 130 },
+  { field: 'status', headerName: 'Order Status', width: 130 },
+  { field: 'status', headerName: 'Order Status', width: 130 },
   { field: 'status', headerName: 'Order Status', width: 130 }
 ];
 
 const rows = [
-  { id: 1, status: 'Ongoing'},
-  { id: 2, status: 'Ongoing'}
+  { id: 1, status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing'},
+  { id: 2, status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing'},
+  { id: 3, status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing'},
+  { id: 4, status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing'},
+  { id: 5, status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing'},
+  { id: 6, status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing'}
 ];
 
+const columnss = [
+  { field: 'id', headerName: 'Order ID', width: 80 },
+  { field: 'customerName', headerName: 'Order Status', width: 130 },
+  { field: 'itemName', headerName: 'Order Status', width: 130 },
+  { field: 'contactNumber', headerName: 'Order Status', width: 130 },
+  { field: 'quantity', headerName: 'Order Status', width: 130 },
+  { field: 'totalAmount', headerName: 'Order Status', width: 130 },
+  { field: 'transactionType', headerName: 'Order Status', width: 130 },
+  { field: 'approvedDate', headerName: 'Order Status', width: 130 }
+];
+
+const rowss = [
+  { id: 1, customerName: 'Ongoing', itemName: 'Ongoing', contactNumber: 'Ongoing', quantity: 'Ongoing', totalAmount: 'Ongoing', transactionType: 'Ongoing', approvedDate: 'Ongoing'},
+  // { id: 2, status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing'},
+  // { id: 3, status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing'},
+  // { id: 4, status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing'},
+  // { id: 5, status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing'},
+  // { id: 6, status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing', status: 'Ongoing'}
+];
 
 function App() {
 	const [navVisible, showNavbar] = useState(false);
@@ -98,13 +127,19 @@ function App() {
 					} />
 					<Route path='/orders' element={
 						<div className={!navVisible ? "page" : "page page-with-navbar"}>
-						 <div style={{ height: 400, width: '100%' }}>
+						 <div style={{ height: 400, width: '50%'}}>
               <DataGrid
                 rows={rows}
                 columns={columns}
-                pageSize={2}
+                pageSize={5}
                 rowsPerPageOptions={[2]}
-                checkboxSelection
+                sx={{marginBottom: 3}}
+              />
+              <DataGrid
+                rows={rowss}
+                columns={columnss}
+                pageSize={5}
+                rowsPerPageOptions={[2]}
               />
             </div>
 						</div>
@@ -113,6 +148,41 @@ function App() {
 						<div className={!navVisible ? "page" : "page page-with-navbar"}>
 							<div>
 								<h1>Dashboard</h1>
+							</div>
+						</div>
+					} />
+          <Route path='/supplies' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<div>
+								<h1>Supplies</h1>
+							</div>
+						</div>
+					} />
+          <Route path='/transactions' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<div>
+								<h1>Transactions</h1>
+							</div>
+						</div>
+					} />
+          <Route path='/pethistory' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<div>
+								<h1>Pet History</h1>
+							</div>
+						</div>
+					} />
+          <Route path='/addnewemployee' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<div>
+								<h1>Add new employee</h1>
+							</div>
+						</div>
+					} />
+          <Route path='/inquiries' element={
+						<div className={!navVisible ? "page" : "page page-with-navbar"}>
+							<div>
+								<h1>Inquiries and messages</h1>
 							</div>
 						</div>
 					} />
