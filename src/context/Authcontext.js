b407e429054a2d55003b4513443fe7d297fb5a23
R-Angelo <114341503+R-Angelo@users.noreply.github.com>
2022-10-26 17:46:49 +0800

    Update Authcontext.js

--- src/context/Authcontext.js
+++ src/context/Authcontext.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { auth } from '../firebase'
+import { auth, fs } from '../firebase'
+import { collection, setDoc } from "firebase/firestore"; 
 
 const Authcontext = React.createContext()
 
@@ -9,11 +10,29 @@ export function useAuth(){
 
 export function AuthProvider({ children }){
     const [currentUser, setCurrentUser] = useState()
+    // 
+    const signup = async (name, email, phoneNum, password, gender) => {
+        try {
+            // auth createuserwithemailandpassword for AUTHENTICATION LOGIN ONLY
+          const res = await auth.createUserWithEmailAndPassword(auth, email, password);
+          const user = res.user;
+          //setDOC function to input data to firestore
+          await setDoc(collection(fs, "users"), {
+            uid: user.uid,
+            name,
+            email,
+            phoneNum,
+            password,
+            gender
+          });
+        } catch (err) {
+          console.error(err);
+          alert(err.message);
+        }
+      };
 
-    function signup(name, email, phonenum, password, gender){
-        return auth.createUserWithEmailAndPassword(name, email, phonenum, password, gender)
-    }
 
+      //onAuthCHANGE if user not null continue to dashboard, if user null stay on login
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
