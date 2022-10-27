fe21af90e2bbdb60548d4aa9cdab238cf5a47aa6
rayveerrr <95725971+rayveerrr@users.noreply.github.com>
2022-10-27 12:49:29 +0800

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { useAuth } from "../context/AuthContext";
 
 // Tools
-import { Avatar, FormControlLabel, Grid, Paper, TextField, Checkbox, Button, Typography, Link } from '@mui/material';
+import { Avatar, FormControlLabel, Grid, Paper, TextField, Checkbox, Button, Typography, Link, Alert } from '@mui/material';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControl from '@mui/material/FormControl';
@@ -12,52 +13,78 @@ import LoginHeader from './LoginHeader'
 
 //icons
 import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import Footer from "./Footer";
 
 const SignUp = () =>{
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const genderRef = useRef()
+    const phoneNumRef = useRef()
+    const passwordRef = useRef()
+    const confirmPassRef = useRef()
+    const { signup, currentUser } = useAuth()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
 
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [phoneNum, setPhoneNum] = useState();
-    const [password, setPassword] = useState();
-    const [cpass, setCPass] = useState();
+    // const [name, setName] = useState();
+    // const [email, setEmail] = useState();
+    // const [phoneNum, setPhoneNum] = useState();
+    // const [password, setPassword] = useState();
+    // const [cpass, setCPass] = useState();
 
     // Error
-    const [nameError, setNameError] = useState();
-    const [emailError, setEmailError] = useState();
-    const [phoneNumError, setPhoneNumError] = useState();
-    const [passwordError, setPasswordError] = useState();
-    const [cpassError, setCPassError] = useState();
+    // const [nameError, setNameError] = useState();
+    // const [emailError, setEmailError] = useState();
+    // const [phoneNumError, setPhoneNumError] = useState();
+    // const [passwordError, setPasswordError] = useState();
+    // const [cpassError, setCPassError] = useState();
 
-    const [gender, setGender] = useState('');
+    // const [gender, setGender] = useState('');
 
-    const handleSubmit = (e) => {
+    async function handleSubmit(e) {
         e.preventDefault()
-        setName(false)
-        setEmail(false)
-        setPhoneNum(false)
-        setPassword(false)
-        setCPass(false)
 
-        if( name == ''){
-            setNameError(true)
-        }
-        if( email == ''){
-            setEmailError(true)
-        }
-        if( phoneNum == ''){
-            setPhoneNumError(true)
-        }
-        if( password == ''){
-            setPasswordError(true)
-        }
-        if( cpass == ''){
-            setCPassError(true)
+        if(passwordRef.current.value !== confirmPassRef.current.value) {
+            return setError('Passwords do not match.')
         }
 
-
-        if (name && email && phoneNum && password && cpass){
-            console.log(name, email, gender, phoneNum, password, cpass)
+        try{
+            setError('')
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+        }catch{
+            setError('Failed to create an account.')
         }
+        setLoading(false)
+
+        
+
+    //     // setName(false)
+    //     // setEmail(false)
+    //     // setPhoneNum(false)
+    //     // setPassword(false)
+    //     // setCPass(false)
+
+    //     // if( name == ''){
+    //     //     setNameError(true)
+    //     // }
+    //     // if( email == ''){
+    //     //     setEmailError(true)
+    //     // }
+    //     // if( phoneNum == ''){
+    //     //     setPhoneNumError(true)
+    //     // }
+    //     // if( password == ''){
+    //     //     setPasswordError(true)
+    //     // }
+    //     // if( cpass == ''){
+    //     //     setCPassError(true)
+    //     // }
+
+
+    //     // if (name && email && phoneNum && password && cpass){
+    //     //     console.log(name, email, gender, phoneNum, password, cpass)
+    //     // }
     }
 
     const paperStyled = {
@@ -87,6 +114,8 @@ const SignUp = () =>{
                 <Typography variant="caption">
                 Please fill up this form to sign up.
                 </Typography>
+                {currentUser && currentUser.email}
+                {error && <Alert severity="warning">{error}</Alert>}
             </Grid>
             <Grid>
                 <form onSubmit={handleSubmit}>
@@ -94,28 +123,32 @@ const SignUp = () =>{
                         variant="outlined" 
                         label='Name' 
                         id="name" 
+                        ref={nameRef}
                         fullWidth
-                        onChange={(e) => setName(e.target.value)}
-                        error={nameError}
+                        // onChange={(e) => setName(e.target.value)}
+                        // error={nameError}
                         required 
                         style={txtFieldStyle} />
                     <TextField 
                         variant="outlined" 
                         label='Email' 
                         id="email" 
+                        ref={emailRef}
                         fullWidth 
-                        onChange={(e) => setEmail(e.target.value)}
-                        error={emailError}
+                        // onChange={(e) => setEmail(e.target.value)}
+                        // error={emailError}
                         required 
                         style={txtFieldStyle}/>
                     <FormControl>
-                        <FormLabel id="gender">Gender</FormLabel>
+                        <FormLabel id="gender" 
+                        ref={genderRef}
+                        >Gender</FormLabel>
                         <RadioGroup
                             row
                             aria-labelledby="gender"
                             name="gender"
-                            value={gender}
-                            onChange={(e) => {setGender(e.target.value)}}
+                            // value={gender}
+                            // onChange={(e) => {setGender(e.target.value)}}
                         >
                             <FormControlLabel value="female" control={<Radio />} label="Female" />
                             <FormControlLabel value="male" control={<Radio />} label="Male" />
@@ -132,32 +165,37 @@ const SignUp = () =>{
                     <TextField 
                         variant="outlined" 
                         label='Phone number' 
-                        id="phoneNum" 
+                        id="phoneNum"
+                        ref={phoneNumRef}
                         fullWidth 
-                        onChange={(e) => setPhoneNum(e.target.value)}
-                        error={phoneNumError}
+                        // onChange={(e) => setPhoneNum(e.target.value)}
+                        // error={phoneNumError}
                         required 
                         style={txtFieldStyle}/>
                     <TextField 
                         variant="outlined" 
                         label='Password' 
                         id="password" 
+                        ref={passwordRef}
                         fullWidth 
-                        onChange={(e) => setPassword(e.target.value)}
-                        error={passwordError}
+                        // onChange={(e) => setPassword(e.target.value)}
+                        // error={passwordError}
                         required 
                         style={txtFieldStyle} />
                     <TextField 
                         variant="outlined" 
                         label='Confirm Password' 
                         id="confirmPassword" 
+                        ref={confirmPassRef}
                         fullWidth
-                        onChange={(e) => setCPass(e.target.value)}
-                        error={cpassError} 
+                        // onChange={(e) => setCPass(e.target.value)}
+                        // error={cpassError} 
                         required 
                         style={txtFieldStyle} />
                     <FormControlLabel control={<Checkbox required/>} label="I accept the terms and conditions"/>
-                    <Button type="submit" variant='contained' fullWidth style={btnStyle}>Sign Up</Button>
+                    <Button type="submit" variant='contained' fullWidth style={btnStyle} 
+                    disabled={loading}
+                    >Sign Up</Button>
                     <hr></hr>
                     <Typography align='center'>
                         Already have an account? <Link href="/login"> Sign in</Link>
@@ -166,6 +204,7 @@ const SignUp = () =>{
             </Grid>
         </Paper>
     </Grid>
+    <Footer />
         </>
     )
 }
